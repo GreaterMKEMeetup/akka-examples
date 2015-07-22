8fcfee57635bb3fa14cf375b2096a2028f2fd61d
andy <andrew.glassman@gmail.com>
2015-07-22 14:35:46 -0500

    Added example of a different message router.

--- akka-test/src/test/java/org/gmjm/akka/webcrawler/UrlCrawlUtilTest.java
+++ akka-test/src/test/java/org/gmjm/akka/webcrawler/UrlCrawlUtilTest.java
@@ -3,19 +3,17 @@ package org.gmjm.akka.webcrawler;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
 import org.gmjm.akka.util.FileWritingManager;
 import org.gmjm.akka.util.SysOutActor;
 import org.junit.Test;
-import org.xml.sax.SAXException;
 
 import scala.concurrent.duration.FiniteDuration;
-import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.routing.RoundRobinPool;
+import akka.routing.SmallestMailboxPool;
 
 public class UrlCrawlUtilTest {
 
@@ -26,25 +24,21 @@ public class UrlCrawlUtilTest {
 	}
 	
 	@Test
-	public void testExtractLinks() throws MalformedURLException, SAXException, IOException {
-//		for(String url : UrlCrawlUtil.crawl("http://jsoup.org/")) {
-//			System.out.println(url);
-//		}
-	}
-	
-	@Test
 	public void testCrawl() throws InterruptedException, IOException {
 		long startTime = System.currentTimeMillis();
 		
 		ActorSystem as = ActorSystem.create("crawler");
 		
-		ActorRef crawlAr = as.actorOf(new RoundRobinPool(4).props(Props.create(CrawlActor.class)),"crawl_crawlActor");
+		Props crawlPropertiesSmallestMailbox = new SmallestMailboxPool(4).props(Props.create(CrawlActor.class));
+		Props crawlPropertiesRoundRobin = new RoundRobinPool(4).props(Props.create(CrawlActor.class));
+		
+		as.actorOf(crawlPropertiesSmallestMailbox,"crawl_crawlActor");
 		as.actorOf(Props.create(CrawlLogger.class),"crawl_crawlLogger");
 		as.actorOf(Props.create(FileWritingManager.class),"output_fileManager");
 		as.actorOf(Props.create(SysOutActor.class),"crawl_sysout");
 		as.actorOf(Props.create(new SearchCollector.SearchCollectorCreator(1)),"output_searchCollector");
 		
-		as.actorSelection("akka://crawler/user/crawl*").tell(new SearchCrawl("http://aglassman.github.io",0,4,"indiana jones",true), crawlAr);
+		as.actorSelection("akka://crawler/user/crawl*").tell(new SearchCrawl("http://aglassman.github.io",0,4,"indiana jones",true), null);
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
